5940611992ec793248bbcc9f63b4321c85406b7e
natumn <natsume.pcuse75@gmail.com>
2018-01-01 21:29:39 +0900

    [Add] main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use 
+
+enum Kind {
+    Print,
+    Lparen,
+    Rparen,
+    Plus,
+    Minus,
+    Multi,
+    Divi,
+    Assgin,
+    VarName,
+    IntNum,
+    EofTkn,
+    Others,
+}
+
+struct Token {
+    kind: Kind,
+    val: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        input();
+        token = token();
+        if (token.kind == EofTkn) {
+            ::std::process::exit(1);
+        }
+        statement();
+    }
 }
